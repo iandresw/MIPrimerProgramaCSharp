68346ef7d7d0a0658139746846d00cf0f8b8d1bb
Andres Bejarano <rbejarano@unah.hn>
2022-12-12 11:49:16 -0600

    first commit

---
+++ MIPrimerProgramaCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIPrimerProgramaCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola, es tiempo de programar");
+            Console.ReadKey();
+        }
+    }
+}
