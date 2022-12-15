24cb7bbbe1ecc1234ba12d37e08693bd10340551
Andres Bejarano <rbejarano@unah.hn>
2022-12-14 23:42:51 -0600

    Objetivo 11 control de excepciones

---
+++ MIPrimerProgramaCSharp/Objetivos11/ControlandoExcepcionescs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace MIPrimerProgramaCSharp.Objetivos11
+{
+    class ControlandoExcepcionescs
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int valor1 = 3;
+                int valor2 = 0;
+                int resultado = valor1 / valor2;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: Divicion por cero");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MIPrimerProgramaCSharp/Objetivos11/TryCatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIPrimerProgramaCSharp.Objetivos11
+{
+    class TryCatch
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------P R O G R A M A   I N I C I A D O-----------");
+            try
+            {
+                int valor1 = 3;
+                int valor2 = 0;
+                int resultado = valor1 / valor2;
+                Console.WriteLine(resultado);
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Error: Divicion por cero ({0})", ex.Message);
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("ERROR General {0}" , ex.Message);
+                Console.WriteLine();
+            }
+            finally
+            {
+                Console.WriteLine("----P R O G R A M A   F I N A L I Z A D O------");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
